f9a296637f6b162f3bd5f4540445573190b3c212
Ingo Speckens <doningo@ymail.com>
2023-09-27 18:21:33 +0200

    Generate_constructor_getter_and_setter_and_fill_main

--- conference-track-management/src/main/java/com/github/brudijoe/ConferenceTrackManagement.java
+++ conference-track-management/src/main/java/com/github/brudijoe/ConferenceTrackManagement.java
@@ -1,17 +1,37 @@
 package com.github.brudijoe;
 
+import java.util.HashSet;
+import com.github.brudijoe.session.Session;
+import com.github.brudijoe.talk.Talk;
+
 /**
  * Main class.
  */
 public final class ConferenceTrackManagement {
 
-    private ConferenceTrackManagement() {
-    }
+    private ConferenceTrackManagement() {}
 
     /**
      * The main method of the application.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Session session = new Session();
+
+        // Create some Talk objects
+        Talk talk1 = new Talk("Talk 1", 60);
+        Talk talk2 = new Talk("Talk 2", 45);
+
+        // Add talks to the session
+        session.addTalk(talk1);
+        session.addTalk(talk2);
+
+        // Retrieve the talks from the session
+        HashSet<Talk> sessionTalks = session.getTalks();
+
+        // Print the talks
+        for (Talk talk : sessionTalks) {
+            System.out.println("Talk Title: " + talk.getName() + ", Duration: " + talk.getDuration()
+                    + " minutes");
+        }
     }
 }
--- conference-track-management/src/main/java/com/github/brudijoe/session/Session.java
+++ conference-track-management/src/main/java/com/github/brudijoe/session/Session.java
@@ -1,6 +1,7 @@
 package com.github.brudijoe.session;
 
 import java.util.HashSet;
+import com.github.brudijoe.talk.Talk;
 
 /**
  * Session class.
@@ -8,6 +9,32 @@ import java.util.HashSet;
 public class Session {
 
     private static final int SESSION_DURATION = 300;
-    private HashSet talks;
+    private HashSet<Talk> talks;
+
+    public Session() {
+        talks = new HashSet<Talk>();
+    }
+
+
+    public HashSet<Talk> getTalks() {
+        return this.talks;
+    }
+
+    public void setTalks(HashSet<Talk> talks) {
+        this.talks = talks;
+    }
+
+    public static int getSessionDuration() {
+        return SESSION_DURATION;
+    }
+
+    public void addTalk(Talk talk) {
+        talks.add(talk);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " talks='" + getTalks() + "'" + "}";
+    }
 
 }
